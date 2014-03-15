4365385a4a49cdda0719bc5cb31e88bb4c9696fa
Bastian Kohlbauer <bastian.kohlbauer@googlemail.com>
2014-03-15 17:01:06 +0100

    Initial Commit

---
+++ StickyHeaderView/HeaderView.h
@@ -0,0 +1,18 @@
+//
+//  HeaderView.h
+//  StickyHeaderView
+//
+//  Created by Bastian Kohlbauer on 15.03.14.
+//  Copyright (c) 2014 Bastian Kohlbauer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeaderView : UIView <UIScrollViewDelegate>
+
+@property (assign, nonatomic) BOOL pageControlUsed;
+@property (strong, nonatomic) NSMutableArray *viewControllers;
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (strong, nonatomic) UIPageControl *pageControl;
+
+@end
---
+++ StickyHeaderView/HeaderView.m
@@ -0,0 +1,154 @@
+//
+//  HeaderView.m
+//  StickyHeaderView
+//
+//  Created by Bastian Kohlbauer on 15.03.14.
+//  Copyright (c) 2014 Bastian Kohlbauer. All rights reserved.
+//
+
+#import "HeaderView.h"
+
+@interface HeaderView ()
+@property (nonatomic, strong) NSArray *imageNames;
+@end
+
+@implementation HeaderView
+
+#pragma mark - Init
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        
+        
+        _imageNames = [NSArray arrayWithObjects:@"iPhone1",@"iPhone2",@"iPhone3",@"iPhone4",@"iPhone5",@"iPhone6", nil];
+        
+        NSMutableArray *controllers = [[NSMutableArray alloc] init];
+        for (unsigned i = 0; i < [_imageNames count]; i++) {
+            [controllers addObject:[NSNull null]];
+        }
+        _viewControllers = controllers;
+        
+        _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, 200)];
+        _scrollView.contentSize = CGSizeMake(_scrollView.frame.size.width * [_imageNames count], _scrollView.frame.size.height);
+        [_scrollView setBackgroundColor:[UIColor clearColor]];
+        _scrollView.pagingEnabled = YES;
+        _scrollView.showsHorizontalScrollIndicator = NO;
+        _scrollView.showsVerticalScrollIndicator = NO;
+        _scrollView.scrollsToTop = NO;
+        _scrollView.autoresizesSubviews = YES;
+        _scrollView.delegate = self;
+        [self addSubview:_scrollView];
+        
+        _pageControl = [[UIPageControl alloc]initWithFrame: CGRectMake(0, 190, self.frame.size.width, 10)];
+        _pageControl.numberOfPages = [_imageNames count];
+        [_pageControl setBackgroundColor:[UIColor clearColor]];
+        [_pageControl setUserInteractionEnabled:NO];
+        [_pageControl setPageIndicatorTintColor:[UIColor lightGrayColor]];
+        [_pageControl setCurrentPageIndicatorTintColor:[UIColor whiteColor]];
+        if ([_imageNames count] > 1) [self addSubview:_pageControl];
+        
+        [self loadScrollViewWithPage:0];
+        [self loadScrollViewWithPage:1];
+    }
+    return self;
+}
+
+#pragma mark - Load ScrollView Pages
+- (void)loadScrollViewWithPage:(int)page {
+    
+    if (page < 0) return;
+    if (page >= [_imageNames count]) return;
+    
+    
+    // replace the placeholder if necessary
+    UIImageView *controller = [_viewControllers objectAtIndex:page];
+    
+    if ((NSNull *)controller == [NSNull null]) {
+        
+        controller = [[UIImageView alloc] init];
+        CGRect frame = _scrollView.frame;
+        frame.origin.x = frame.size.width * page;
+        frame.origin.y = 0;
+        controller.frame = frame;
+        [controller setContentMode:UIViewContentModeScaleAspectFill];
+        controller.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        [controller.layer setMasksToBounds:YES];
+        [_scrollView addSubview:controller];
+        
+        UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        [spinner setCenter:CGPointMake(controller.center.x, controller.center.y)];
+        [spinner startAnimating];
+        [controller addSubview:spinner];
+        
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            
+            for (int i = 0; i < [_imageNames count]; i++) {
+                
+                if (page == i) { //set up each page
+                    
+                    [_viewControllers replaceObjectAtIndex:page withObject:controller];
+                    
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        
+                        [controller setImage:[UIImage imageNamed:[NSString stringWithFormat:@"Resources.bundle/%@.png",[_imageNames objectAtIndex:i]]]];
+                        [spinner removeFromSuperview];
+                        
+                        return;
+                        
+                    });
+                }
+            }
+            
+        });
+        
+        
+        
+    }
+    
+    
+}
+
+#pragma mark - ScrollView Methods
+- (void)scrollViewDidScroll:(UIScrollView *)sender {
+    if (_pageControlUsed) {
+        return;
+    }
+    
+    CGFloat pageWidth = _scrollView.frame.size.width;
+    int page = floor((_scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+    _pageControl.currentPage = page;
+    
+    [self loadScrollViewWithPage:page - 1];
+    [self loadScrollViewWithPage:page];
+    [self loadScrollViewWithPage:page + 1];
+    
+    // A possible optimization would be to unload the views+controllers which are no longer visible
+}
+// At the begin of scroll dragging, reset the boolean used when scrolls originate from the UIPageControl
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    _pageControlUsed = YES;
+}
+
+// At the end of scroll animation, reset the boolean used when scrolls originate from the UIPageControl
+- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView
+                     withVelocity:(CGPoint)velocity
+              targetContentOffset:(inout CGPoint *)targetContentOffset
+{
+    _pageControlUsed = NO;
+}
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
